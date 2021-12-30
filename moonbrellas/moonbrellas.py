72415a2c4e713eaf0338cfd58e261288e841c960
NelsonLAN <nelsonlan199601@gmail.com>
2021-12-30 16:53:14 +0800

    Backup 30Dec2021 1652
    School

--- moonbrellas/moonbrellas.py
+++ moonbrellas/moonbrellas.py
@@ -1,36 +1,32 @@
-import random
+def moonbrellas_main():
+    x_cost = [0]* 999
+    y_cost = [0] * 999
+    str_arg = [""] * 999
+    test_cases = int(input())
+    case_no = 1
 
+    for i in range(test_cases):
+        x, y, s = input().split()
+        x_cost[i] = int(x)
+        y_cost[i] = int(y)
+        str_arg[i] = str(s)
 
-def moonbrellas():
-    test_cases = input()
+        # insert "?" replacement function (call) here
 
-    x, y, s = input().split()
-    x = int(x)
-    y = int(y)
-    s = str(s)
-    s_i = [None]*1000
+    for i in range(test_cases):
+        calculate_cost(x_cost[i], y_cost[i], str_arg[i],case_no)
+        case_no+=1
+
+def calculate_cost(x, y, s, case_no):
     x_count = 0
     y_count = 0
-    print(s)
-    for i in range(len(s)):
-        if s[i] == "C":
-            s_i[i] = 0
-        elif s[i] == "J":
-            s_i[i] == 1
-        elif s[i] == "?":
-            choose = "01"
-            s_i[i] = random.choice(choose)
-
-
-    for i in range(len(s)-1):
-        if s[i] == "C" and s[i+1] == "J":
+    for i in range(len(s) - 1):
+        if s[i] == "C" and s[i + 1] == "J":
             x_count += 1
-        elif s[i] == "J" and s[i+1] == "C":
+        elif s[i] == "J" and s[i + 1] == "C":
             y_count += 1
-    print(x_count,y_count)
     cost = x_count * x + y_count * y
-    print("cost ==> ", cost)
+    print("Case #", case_no, ": ", cost)
 
 
-moonbrellas()
-
+moonbrellas_main()
