0f1e3a6f1697a318a2546960ded1a0fdfafcf8e1
NelsonLAN <nelsonlan199601@gmail.com>
2022-01-29 11:44:22 +0800

    Add files via upload

--- google_kick_2020A.py
+++ google_kick_2020A.py
@@ -1,16 +1,37 @@
-house = [0] * 99999
-
+house = []
+global int_house
+int_house= []
 test_case = int(input())
-bought_house = 0
+global bought
+bought = 0
+bought_arg = []
 
+def convert_house(house):
+    for i in range(len(house)):
+        int_house.append(int(house[i]))
+    return int_house
+
+def initial_var():
+    int_house.clear()
+    nth_house_price.clear()
+
+def print_result():
+    for i in range(len(bought_arg)):
+        print("Case #",i+1,":",bought_arg[i])
+        
 for i in range(test_case):
     n, b = input().split()
     n = int(n)
     b = int(b)
     nth_house_price = input().split()
     nth_house_price.sort()
-    print(nth_house_price)
-    while b >= nth_house_price[0] :
-        bought_house += 1
-        nth_house_price.pop(0)
-    print(nth_house_price)
+    int_house = convert_house(nth_house_price)
+    bought = 0
+    while b >= int_house[0]:
+        bought += 1
+        b -= int_house[0]
+        int_house.pop(0)
+    bought_arg.append(bought)
+    initial_var()
+print_result()
+    
